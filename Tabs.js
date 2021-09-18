2ff784820f004e4ea1625cfb1f6cbc935064f40b
jacqw8 <jacquelinewu88@gmail.com>
2021-09-18 14:55:36 -0500

    new bottom tab request

--- Tabs.js
+++ Tabs.js
@@ -3,6 +3,7 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Chat from "./screens/Chat";
 import StudyGroup from "./screens/StudyGroup";
 import Account from "./screens/Account";
+import Request from "./screens/Request"
 
 const Tab = createBottomTabNavigator();
 
@@ -11,6 +12,7 @@ const Tabs = () => {
     <Tab.Navigator>
       <Tab.Screen name="Chat" component={Chat} />
       <Tab.Screen name="Study Groups" component={StudyGroup} />
+      <Tab.Screen name="Join" component={Request} />
       <Tab.Screen name="Account" component={Account} />
     </Tab.Navigator>
   );
