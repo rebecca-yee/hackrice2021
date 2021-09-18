07f4a6f46c655aab4b4426b71a6217210fba7c8b
Rebecca Yee <rmy3@rice.edu>
2021-09-18 13:23:07 -0500

    tabs

--- App.js
+++ App.js
@@ -7,7 +7,6 @@ import { NavigationContainer } from "@react-navigation/native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
       <NavigationContainer>
         <Tabs />
