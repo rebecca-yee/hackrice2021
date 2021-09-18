fb9a3613e82674cf07ac9b3cd97abf26daf61bc3
jacqw8 <jacquelinewu88@gmail.com>
2021-09-18 13:14:01 -0500

    screens

---
+++ components/Find.js
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from 'react';
+import { Platform, StyleSheet, Text, View, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+
+// components
+
+class Find extends React.Component {
+
+
+    render() {
+        const statusbar = (Platform.OS == 'ios') ? <View style={styles.statusbar}></View> : <View></View>
+
+        return (
+            <View style={styles.container} >
+                {statusbar}
+                < Header />
+                <View style={styles.balance}>
+
+                </View>
+                <View style={styles.input}>
+                    <Text>Find Friends</Text>
+                </View>
+                <History />
+
+            </View >
+        )
+    }
+}
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#EDF8F2',
+    },
+    statusbar: {
+        backgroundColor: 'rgb(192, 165, 192)',
+        height: 50
+    },
+    balance: {
+        paddingBottom: 20
+    },
+    input: {
+        paddingBottom: 20,
+        alignItems: 'center'
+    }
+});
+
+export default Find
