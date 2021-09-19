f3a92fc68344e366fb88cb65b60581606701c12d
theoschweizer <schweizer.theodore@gmail.com>
2021-09-18 22:37:15 -0500

    chat

--- screens/Chat.js
+++ screens/Chat.js
@@ -1,28 +1,42 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useCallback, useEffect } from 'react'
 import { Platform, StyleSheet, Text, View, Button } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import { GiftedChat } from 'react-native-gifted-chat'
 
 // components
 
-class Find extends React.Component {
-  render() {
-    const statusbar =
-      Platform.OS == "ios" ? (
-        <View style={styles.statusbar}></View>
-      ) : (
-        <View></View>
-      );
+export function Find() {
+  const [messages, setMessages] = useState([]);
 
-    return (
-      <View style={styles.container}>
-        <View style={styles.balance}></View>
-        <View style={styles.input}>
-          <Text>My Chats</Text>
-        </View>
-      </View>
-    );
-  }
+  useEffect(() => {
+    setMessages([
+      {
+        _id: 1,
+        text: 'Hi would you be willing to study?',
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: 'React Native',
+          avatar: 'https://placeimg.com/140/140/any',
+        },
+      },
+    ])
+  }, [])
+
+  const onSend = useCallback((messages = []) => {
+    setMessages(previousMessages => GiftedChat.append(previousMessages, messages))
+  }, [])
+
+  return (
+    <GiftedChat
+      messages={messages}
+      onSend={messages => onSend(messages)}
+      user={{
+        _id: 1,
+      }}
+    />
+  )
 }
 
 const styles = StyleSheet.create({
